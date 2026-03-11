3dac721a84caa1f595b5645a59608867bea420e4
johnduartemoreno <johnduartemoreno@gmail.com>
2026-03-10 20:43:37 -0500

    claude: refactor: remove deprecated goal mocks and clean up dead code

--- lib/features/goals/domain/repositories/goals_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:fpdart/fpdart.dart';
-import '../../../../core/errors/failures.dart';
-import '../entities/goal.dart';
-
-abstract class GoalsRepository {
-  Future<Either<Failure, List<Goal>>> getGoals();
-  Future<Either<Failure, Goal>> addGoal(
-      String name, double targetAmount, DateTime deadline);
-}
